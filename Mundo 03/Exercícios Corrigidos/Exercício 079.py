346abad14f21b4adfb2051c33266028c95495f6b
AlexsandroJS <69006993+AlexsandroJS@users.noreply.github.com>
2020-07-30 17:00:15 +0100

    Update Exercício 079.py
    corrigido a questao de passar mesmo com resposta errada!

--- Mundo 03/Exercícios Corrigidos/Exercício 079.py
+++ Mundo 03/Exercícios Corrigidos/Exercício 079.py
@@ -19,3 +19,22 @@ while True:
 print('-=' * 30)
 numeros.sort()
 print(f'Você digitou os valores {numeros}')
+
+############# adicionando essse abaixo para voce comparar e testar, observe que quando voce digita algum numero no lugar da resposta acaba aceitando 
+############# eu fiz outro while para que nao aceite ate que seja dado a resposta correta, da uma olhadinha
+lista = []
+while True:
+    valor = int(input('Digite um valor: '))
+    if valor not in lista:
+        print(f'{valor} adicionado com sucesso')
+        lista.append(valor)
+    else:
+        print('Valor duplicado! Nao vou adicionar...')
+    resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+    while resp not in 'SsNn':
+        resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+    if resp == 'N':
+        break
+lista.sort()
+print('-=' *30)
+print(f'Os valores digitados foram {lista}')
